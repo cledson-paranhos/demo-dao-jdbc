72e4c3319b0f8f4ad4d01cc46dfd055e5eaccbff
cledson-paranhos <cledsonvgp@hotmail.com>
2025-10-30 17:23:22 -0300

    Reusing Instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,8 @@ public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = SellerFactory.createSellerDao();
 
+        System.out.println("Seller FindById:");
+
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
